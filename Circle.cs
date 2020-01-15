dbde21eeaf93c02fe1ddcdc23e6a296715056d7c
babak_ss <ssalimib@gmail.com>
2020-01-15 13:07:06 +0330

    initial comit

---
+++ Circle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace DrawShapeTool
+{ 
+    public class Circle
+    {
+        public Vector3 center;
+        public float r;
+    }
+}
---
+++ DrawShapeEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DrawShapeTool
+{
+    [CustomEditor(typeof(DrawShape))]
+    public class DrawShapeEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawShape drawShape = (DrawShape)target;
+
+            drawShape.SegmentsNum = EditorGUILayout.IntField(new GUIContent("Segments Num"), drawShape.SegmentsNum);
+            drawShape._SegmentsT = EditorGUILayout.FloatField(new GUIContent("Segments T"), drawShape._SegmentsT);
+            drawShape.MainCircleRadius = EditorGUILayout.IntField(new GUIContent("Radius"), drawShape.MainCircleRadius);
+
+            if(GUILayout.Button("Draw"))
+            {
+                drawShape.Draw();
+            }
+        }
+    }
+}
