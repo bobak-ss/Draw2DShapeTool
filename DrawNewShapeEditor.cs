f32d76ec35234074b8a87f949944582a1f23d891
babak_ss <ssalimib@gmail.com>
2020-01-16 15:36:45 +0330

    DrawShape changed to DrawNewShape

--- DrawNewShapeEditor.cs
+++ DrawNewShapeEditor.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 
 namespace DrawShapeTool
 {
-    [CustomEditor(typeof(DrawShape))]
-    public class DrawShapeEditor : Editor
+    [CustomEditor(typeof(DrawNewShape))]
+    public class DrawNewShapeEditor : Editor
     {
         public override void OnInspectorGUI()
         {
-            DrawShape drawShape = (DrawShape)target;
+            DrawNewShape drawShape = (DrawNewShape)target;
 
             drawShape.segmentsNum = EditorGUILayout.IntField(new GUIContent("Segments Num"), drawShape.segmentsNum);
             drawShape.segmentT = EditorGUILayout.FloatField(new GUIContent("Segments T"), drawShape.segmentT);
             drawShape.radius = EditorGUILayout.FloatField(new GUIContent("Radius"), drawShape.radius);
 
-            if(GUILayout.Button("Draw"))
+            if(GUILayout.Button("Draw New Shape"))
             {
                 drawShape.Draw();
             }
